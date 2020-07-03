e48d729036e260cf1333e4decef8f731d5276dfb
Henrik Auzins <37082293+henrikauzins@users.noreply.github.com>
2020-07-03 11:42:58 +0100

    added new OrderingSystem function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#importing py classes
+#importing external py classes
 import employee
 import customer
 import base
@@ -10,29 +10,36 @@ import pizza
 
 def Employee():
     print("this is the employee interface")
+    name = input("what is your name: ")
     rank = input("what is your rank: ")
     new_employee = employee.Employee(name, rank)
     print(new_employee)
 
 def Customer():
+
+    print("this is the customer interface")
+    name = input("what is your name: ")
+    # creating a new Customer object
+    new_customer = customer.Customer(name)
+    print("Customer Name: ", new_customer.name)
+
+    #calls the ordering system function
+    OrderingSystem()
+
+
+
+def OrderingSystem():
     ## customer_order will be storing the objects of the pizza order
     customer_order = []
     location = ["london", "newcastle", "bath", "liverpool", "kent"]
     orderNumber = 0
-    print("this is the customer interface")
-    new_customer = customer.Customer(name)
-    print(new_customer.name)
-
     pizza_name = input("what pizza would you like?")
 
-
     pizza_type = input("what pizza type would you like?")
-
 
     new_pizza = pizza.Pizza(pizza_name, pizza_type)
 
     customer_order.append(new_pizza)
-
 
     pizza_topping = input("what pizza topping would you like?")
     new_topping = topping.Topping(pizza_topping)
@@ -40,7 +47,7 @@ def Customer():
 
     pizza_base = input("what size pizza base would you like?")
     customer_order.append(pizza_base)
-    #new base object initialised
+    # new base object initialised
     new_base = base.Base(pizza_base)
     new_base.GetBase(pizza_base)
 
@@ -65,15 +72,33 @@ def Customer():
     print("Order Summary")
     print(customer_order)
 
+    place_order = input("would you like to place your order: ")
+
+    if place_order == "yes":
+        print("order has been placed")
+        main()
+    elif place_order == "no":
+        print("order has been deleted")
+        customer_order.clear()
+        main()
 
 
+## main function
+def main():
 
-name = input("what is your name: ")
-user_type = input("are you a customer or employee: ")
+    while True:
 
-if user_type == "customer" or "customer":
-    Customer()
+        user_type = input("are you a customer or employee: ")
 
+
+        if user_type == "customer":
+            Customer()
+        elif user_type == "employee":
+            Employee()
+
+## runs main() function
+
+main()
 
 
 
